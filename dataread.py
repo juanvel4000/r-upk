675ad8ac32de1089ad5e38a56a72f8fb228ec801
juanvel4000 <juanvel400@proton.me>
2024-12-25 21:42:52 -0500

    Created a function for Creating Packages using a working directory for base

---
+++ dataread.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from configparser import ConfigParser
+def parsemanifest(file):
+    if not os.path.isfile(file):
+        print(f"Could not find {file}")
+        return None
+    dictionary = {}
+    config = ConfigParser()
+    try:
+        config.read(file)
+    except MissingSectionHeaderError:
+        print(f"Error: File '{file}' is not a valid manifest.")
+        return None
+    if not config.has_section("Package"):
+        print("Manifest parsing error: could not find the \"Package\" section")
+        return False
+    options = ["Name", "Version", "Maintainer"]
+    for option in options:
+        if not config.has_option("Package", option):
+            print(f"Could not find {option}")
+            return False
+        dictionary[option] = config['Package'][option]
+    optionals = ["Summary", "Dependencies", "PostInstall", "PreInstall", "Uninstall", "Section"]
+    for option in optionals:
+        if config.has_option("Package", option):
+            dictionary[option] = config['Package'][option]
+    return dictionary
+
+def add_entry(root):
+    if not os.path.isdir(f'{root}/etc/rupk'):
+        os.makedirs(f'{root}/etc/rupk')
+    with open(f'{root}/etc/rupk/packages.db', 'a') as db:
+        db.write(f'{name}:{version}\n')
+    return True
--- package.py
+++ package.py
@@ -1,29 +1,72 @@
+import tarfile
 import os
-import sys
-from configparser import ConfigParser
-def parsemanifest(file):
-    if not os.path.isfile(file):
-        print(f"Could not find {file}")
-        return None
-    dictionary = {}
-    config = ConfigParser()
-    try:
-        config.read(file)
-    except MissingSectionHeaderError:
-        print(f"Error: File '{file}' is not a valid manifest.")
-        return None
-    if not config.has_section("Package"):
-        print("Manifest parsing error: could not find the \"Package\" section")
+import dataread
+def create_package(workdir):
+    cdir = os.getcwd()
+    if not os.path.isdir(workdir):
+        print("Error: the Working directory doesnt exist")
         return False
-    options = ["Name", "Version", "Maintainer"]
-    for option in options:
-        if not config.has_option("Package", option):
-            print(f"Could not find {option}")
-            return False
-        dictionary[option] = config['Package'][option]
-    optionals = ["Summary", "Dependencies", "PostInstall", "PreInstall", "Uninstall", "Section"]
-    for option in optionals:
-        if config.has_option("Package", option):
-            dictionary[option] = config['Package'][option]
-    return dictionary
-
+    workdir = os.path.abspath(workdir)
+    os.chdir(workdir)
+    os.mkdir(f'{workdir}/RUPK-OUTPUT')
+    if not os.path.isdir(f'{workdir}/RUPK'):
+        print("Could not find a RUPK directory")
+        return False
+    if not os.path.isfile(f'{workdir}/RUPK/Manifest.ini'):
+        print("Could not find the Manifest file in RUPK/Manifest.ini file.")
+        return False
+    manifest = dataread.parsemanifest(f'{workdir}/RUPK/Manifest.ini')
+    print(f"Creating {manifest['Name']}...")
+    if manifest == False:
+        print("An error occurred during Manifest parsing")
+        return False
+    os.chdir(workdir)
+    with open(f"{workdir}/RUPK/tree", 'w') as tree:
+        for root, dirs, files in os.walk(workdir):
+            if 'RUPK' in dirs:
+                dirs.remove('RUPK')
+            if 'RUPK-OUTPUT' in dirs:
+                dirs.remove('RUPK-OUTPUT')
+            if files:
+                for file in files:
+                    full_path = os.path.join(root, file)
+                    if full_path.startswith(workdir):
+                        full_path = full_path[len(workdir):]  
+                    tree.write(f'{full_path}\n')
+    os.chdir(f"{workdir}/RUPK")
+    with tarfile.open(f'{workdir}/RUPK-OUTPUT/info.tar.xz', 'w|xz') as txz:
+        txz.add('Manifest.ini')
+        txz.add('tree')
+        if "PostInstall" in manifest:
+            if not os.path.isfile(f'{workdir}/RUPK/{manifest['PostInstall']}'):
+                print("Could not find the postinstall script")
+                return False
+            txz.add(manifest['PostInstall'])
+        if "PreInstall" in manifest:
+            if not os.path.isfile(f'{workdir}/RUPK/{manifest['PreInstall']}'):
+                print("Could not find the preinstall script")
+                return False
+            txz.add(manifest['PreInstall'])
+        if "Uninstall" in manifest:
+            if not os.path.isfile(f'{workdir}/RUPK/{manifest['Uninstall']}'):
+                print("Could not find the Uninstall script")
+                return False
+            txz.add(manifest['Uninstall'])
+    os.chdir(workdir)
+    with tarfile.open(f'{workdir}/RUPK-OUTPUT/data.tar.xz', 'w|xz') as txz:
+        for line in f'{workdir}/RUPK/tree':
+                file_path = line.strip()
+                
+                if os.path.isfile(file_path):
+                    txz.add(file_path, arcname=os.path.basename(file_path))
+    os.chdir(f'{workdir}/RUPK-OUTPUT')
+    with tarfile.open(f'{cdir}/{manifest['Name']}-{manifest['Version']}.rupk', 'w|xz') as txz:
+        txz.add(f'{workdir}/RUPK-OUTPUT/data.tar.xz', arcname="data.txz")
+        txz.add(f"{workdir}/RUPK-OUTPUT/info.tar.xz", arcname="info.txz")
+    os.remove(f'{workdir}/RUPK-OUTPUT/data.tar.xz')
+    os.remove(f'{workdir}/RUPK-OUTPUT/info.tar.xz')
+    os.rmdir(f'{workdir}/RUPK-OUTPUT')
+    print(f"Successfully built {manifest['Name']}-{manifest['Version']}.rupk")
+    os.chdir(cdir)
+    return True
+create_package('package/')
